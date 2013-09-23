e4fce1011d54bc66b2192f9ce629accef2159b14
Marc DiBlasi <marc@dibla.si>
2013-09-22 23:02:45 -0500

    Add guts to echo parser

--- lib/Limey/Parsers/Echo.php
+++ lib/Limey/Parsers/Echo.php
@@ -1,5 +1,11 @@
 <?php
 
 class Limey_Parsers_Echo extends Limey_Parser {
+    protected $affects = array('T_ECHO');
     
+    public function parse($input) {
+        $input[1] = 'announce';
+
+        return $input;
+    }
 }
