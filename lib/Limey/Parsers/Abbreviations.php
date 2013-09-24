dad8b12cbdd15488b93292a58132694fbf0ae393
Marc DiBlasi <marc@dibla.si>
2013-09-23 22:01:37 -0500

    Add a ton of abbreviations.

--- lib/Limey/Parsers/Abbreviations.php
+++ lib/Limey/Parsers/Abbreviations.php
@@ -6,20 +6,70 @@ class Limey_Parsers_Abbreviations extends Limey_Parser {
     public function parse($input) {
         $input[1] = str_replace(
             array(
+                'string_get_comma_separated_values',
+                'string_ireplace',
+                'string_pad',
+                'string_repeat',
                 'string_replace',
+                'string_rot13',
+                'string_shuffle',
+                'string_split',
+                'string_word_count',
+
                 'is_integer',
+                
                 'variable_dump',
-                'perl_regular_expression_match',
+                'variable_export',
+                
+                'javascript_object_notation_decode',
                 'javascript_object_notation_encode',
+                'javascript_object_notation_last_error_msg',
+                'javascript_object_notation_last_error',
+                
                 'my_structured_query_language_connect',
+                
+                'perl_regular_expression_filter',
+                'perl_regular_expression_grep',
+                'perl_regular_expression_last_error',
+                'perl_regular_expression_match_all',
+                'perl_regular_expression_match',
+                'perl_regular_expression_quote',
+                'perl_regular_expression_replace_callback',
+                'perl_regular_expression_replace',
+                'perl_regular_expression_split',
             ),
             array(
+                'str_getcsv',
+                'str_ireplace',
+                'str_pad',
+                'str_repeat',
                 'str_replace',
+                'str_rot13',
+                'str_shuffle',
+                'str_split',
+                'str_word_count',
+
                 'is_int',
+
                 'var_dump',
-                'preg_match',
+                'var_export',
+                
+                'json_decode',
                 'json_encode',
+                'json_last_error_msg',
+                'json_last_error',
+                
                 'mysql_connect',
+                
+                'preg_filter',
+                'preg_grep',
+                'preg_last_error',
+                'preg_match_all',
+                'preg_match',
+                'preg_quote',
+                'preg_replace_callback',
+                'preg_replace',
+                'preg_split',
             ),
             $input[1]
         );
